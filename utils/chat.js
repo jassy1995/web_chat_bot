b46c51179dc65dac89ed7eda40650e547d5ff953
jassy1995 <babatundejoseph85@gmail.com>
2022-01-11 22:00:43 +0100

    edited the account detail reusingdjdjd

--- utils/chat.js
+++ utils/chat.js
@@ -7,7 +7,7 @@ const {
 const { productsButtons, listButtons } = require("./interactive_button");
 
 const question_one = {
-  full_name: "Kindly enter your full name",
+  full_name: "Kindly enter your *full name*",
   service:
     "Kindly click the button below to choose the service you are offering",
   state: "What state do you reside ?",
