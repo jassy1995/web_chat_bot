b8bfd6916fd36e4b7c4f349259758834ad2496c8
jassy1995 <babatundejoseph85@gmail.com>
2022-01-11 21:58:08 +0100

    edited the account detail reusingdjdjd

--- controllers/process.controller.js
+++ controllers/process.controller.js
@@ -31,16 +31,8 @@ const {
 } = require("../utils/query");
 
 exports.RegistrationProcess = async (req, res) => {
-  // const {
-  //   payload: {
-  //     type,
-  //     text: message,
-  //     user: { id, name, image },
-  //   },
-  // } = req.body;
   const { payload } = req.body;
   let response;
-  let result;
 
   try {
     const stage = await currentStage(payload.user.id);
@@ -57,7 +49,6 @@ exports.RegistrationProcess = async (req, res) => {
       await destroy({ where: { user_id: payload.user.id } });
       await create({ user_id: payload.user.id, step: 1 });
       response = await sendResponse(welcomeResponse, payload.user.id);
-      // response = welcomeResponse;
     } else if (
       payload.type === "text" &&
       stage?.step === 1 &&
@@ -78,7 +69,6 @@ exports.RegistrationProcess = async (req, res) => {
       );
       let fn = await fullNameResponse();
       response = await sendResponse(fn, payload.user.id);
-      // response = await fullNameResponse();
     } else if (stage?.menu === "Render Service (Artisan)") {
       if (payload.type === "text" && stage.step === 2) {
         await update(
@@ -91,7 +81,6 @@ exports.RegistrationProcess = async (req, res) => {
         );
         let rfs = await serviceResponse();
         response = await sendResponse(rfs, payload.user.id);
-        // response = await serviceResponse();
       } else if (
         payload.type === "text" &&
         stage.step === 3 &&
@@ -107,7 +96,6 @@ exports.RegistrationProcess = async (req, res) => {
         );
         let rRe = await stateResponse();
         response = await sendResponse(rRe, payload.user.id);
-        // response = await stateResponse();
       } else if (
         payload.type === "text" &&
         stage?.step === 4 &&
@@ -131,7 +119,6 @@ exports.RegistrationProcess = async (req, res) => {
           }
         );
         response = await sendResponse(info.rests, payload.user.id);
-        // response = info.rest;
       } else if (
         payload.type === "text" &&
         stage?.step === 5 &&
@@ -151,7 +138,6 @@ exports.RegistrationProcess = async (req, res) => {
           }
         );
         response = await sendResponse(otherResponse.address, payload.user.id);
-        // response = otherResponse.address;
       } else if (payload.type === "text" && stage.step === 6) {
         await update(
           { address: payload.text, step: 7 },
@@ -162,8 +148,6 @@ exports.RegistrationProcess = async (req, res) => {
           }
         );
         response = await sendResponse(otherResponse.id_card, payload.user.id);
-        // response = otherResponse.id_card;
-        // info.res;
       } else if (payload.type === "image" && stage?.step === 7) {
         await update(
           { id_card: payload.user.image, step: 8 },
@@ -175,7 +159,6 @@ exports.RegistrationProcess = async (req, res) => {
         );
 
         response = await sendResponse(otherResponse.picture, payload.user.id);
-        // response = otherResponse.picture;
       } else if (payload.type === "image" && stage?.step === 8) {
         await update(
           {
@@ -189,15 +172,6 @@ exports.RegistrationProcess = async (req, res) => {
             },
           }
         );
-        // await update(
-        //   { local_government: acct_value.data },
-        //   {
-        //     where: {
-        //       user_id: payload.user.id,
-        //     },
-        //   }
-        // );
-        console.log(stage.lga);
         const summary = `Name: *${stage.full_name}*, Service: *${
           stage.service
         }*, State: *${stage.state}*,Local_government: *${
@@ -219,7 +193,6 @@ exports.RegistrationProcess = async (req, res) => {
         ];
         let re = productsButtons({ header, summary }, button);
         response = await sendResponse(re, payload.user.id);
-        // response = re;
       } else if (
         payload.text.toString() === "1" &&
         payload.type === "text" &&
@@ -243,11 +216,8 @@ exports.RegistrationProcess = async (req, res) => {
           await createArtisan(toSave);
           let resp = "Congratulation, your registration has been completed";
           response = await sendResponse(resp, payload.user.id);
-          // response = resp;
         } else {
           const newData = await currentStage(payload.user.id);
-          // console.log(newData.lga);
-          // console.log(newData.local_government);
           const summary2 = `kindly make a payment of *${account.formatMoney(
             Number(JSON.parse(newData.local_government).amount),
             "₦"
@@ -283,7 +253,6 @@ exports.RegistrationProcess = async (req, res) => {
         );
         let ress = await serviceResponse();
         response = await sendResponse(ress, payload.user.id);
-        // response = ress;
       } else if (
         payload.type === "text" &&
         stage.step === 3 &&
@@ -299,7 +268,6 @@ exports.RegistrationProcess = async (req, res) => {
         );
 
         response = await sendResponse(otherResponse.address, payload.user.id);
-        // response = otherResponse.address;
       } else if (payload.type === "text" && stage.step === 4) {
         await update(
           { address: payload.text, step: 5 },
@@ -311,7 +279,6 @@ exports.RegistrationProcess = async (req, res) => {
         );
 
         response = await sendResponse(otherResponse.location, payload.user.id);
-        // response = otherResponse.location;
       } else if (payload.type === "location" && stage.step === 5) {
         let location = {
           long: payload.location.longitude,
@@ -329,7 +296,6 @@ exports.RegistrationProcess = async (req, res) => {
           otherResponse.task_description,
           payload.user.id
         );
-        // response = otherResponse.task_description;
       } else if (payload.type === "text" && stage.step === 6) {
         await update(
           { task_description: payload.text, step: 7 },
@@ -339,10 +305,8 @@ exports.RegistrationProcess = async (req, res) => {
             },
           }
         );
-        // states.includes(states[Number(payload.text) - 1]
         let js = await artisanResponse();
         response = await sendResponse(js, payload.user.id);
-        // response = js;
       } else if (
         payload.type === "text" &&
         stage.step === 7 &&
@@ -375,387 +339,18 @@ exports.RegistrationProcess = async (req, res) => {
           "Congrats,your request has been received",
           payload.user.id
         );
-        // response = "Congrats,your request has been received";
       } else {
         let sg =
           "Invalid input,please check and retry or enter *restart* to start all over";
         response = await sendResponse(sg, payload.user.id);
-        // response = sg;
       }
     } else {
       let fg =
         "Invalid input,please check and retry or enter *restart* to start all over";
       response = await sendResponse(fg, payload.user.id);
-      // response = fg;
     }
     return res.status(200).json(response);
   } catch (error) {
     console.log(error);
   }
-  //   // {
-
-  //   // const {
-  //   //   payload: {
-  //   //     type,
-  //   //     text: text,
-  //   //     user: { id, name, image },
-  //   //   },
-  //   // } = req.body;
-  //   let response;
-  //   // console.log(user);
-
-  //   try {
-  //     const stage = await currentStage(payload.user.id);
-  //     const service = await getServices();
-  //     const states = await getStates();
-  //     console.log(stage);
-
-  //     //initialize stage
-  //     if (
-  //       (payload.text.toLowerCase() == "hi" ||
-  //         payload.text.toLowerCase() == "restart") &&
-  //       payload.type === "text"
-  //     ) {
-  //       await destroy({ where: { user_id: payload.user.id } });
-  //       await create({ user_id: payload.user.id, step: 1 });
-  //       response = welcomeResponse;
-  //     } else if (stage?.step === 1 && payload.type === "text") {
-  //       await update(
-  //         { menu: otherResponse.initService[Number(text) - 1], step: 2 },
-  //         {
-  //           where: {
-  //             user_id: id,
-  //           },
-  //         }
-  //       );
-  //       response = await fullNameResponse();
-  //     } else if (stage?.menu === "Render Service (Artisan)") {
-  //       if (
-  //         typeof payload.text === "string" &&
-  //         stage?.step === 2 &&
-  //         payload.type === "text"
-  //       ) {
-  //         await update(
-  //           { full_name: payload.text, step: 3 },
-  //           {
-  //             where: {
-  //               user_id: id,
-  //             },
-  //           }
-  //         );
-  //         response = await serviceResponse();
-  //       } else if (
-  //         payload.type === "text" &&
-  //         stage?.step === 3 &&
-  //         service.includes(service[Number(text) - 1])
-  //       ) {
-  //         await update(
-  //           { service: service[Number(text) - 1], step: 4 },
-  //           {
-  //             where: {
-  //               user_id: payload.user.id,
-  //             },
-  //           }
-  //         );
-  //         response = await stateResponse();
-  //       } else if (
-  //         stage?.step === 4 &&
-  //         states.includes(states[Number(text) - 1])
-  //       ) {
-  //         await update(
-  //           { state: states[Number(payload.text) - 1].name, step: 5 },
-  //           {
-  //             where: {
-  //               user_id: payload.user.id,
-  //             },
-  //           }
-  //         );
-  //         let info = await lgaResponse(states[Number(payload.text) - 1].id);
-  //         response = info.res;
-  //         await update(
-  //           { local_government: info.lg },
-  //           {
-  //             where: {
-  //               user_id: payload.user.id,
-  //             },
-  //           }
-  //         );
-  //       } else if (
-  //         type === "text" &&
-  //         stage?.step === 5 &&
-  //         stage.local_government.includes(
-  //           stage.local_government[Number(payload.text) - 1]
-  //         )
-  //       ) {
-  //         await update(
-  //           {
-  //             lga: stage.local_government[Number(payload.text) - 1].name,
-  //             step: 6,
-  //           },
-  //           {
-  //             where: {
-  //               user_id: payload.user.id,
-  //             },
-  //           }
-  //         );
-  //         response = otherResponse.address;
-  //       } else if (type === "text" && stage.step === 6) {
-  //         await update(
-  //           { address: payload.text, step: 7 },
-  //           {
-  //             where: {
-  //               user_id: id,
-  //             },
-  //           }
-  //         );
-  //         response = otherResponse.id_card;
-  //       } else if (payload.type === "image" && stage?.step === 7) {
-  //         await update(
-  //           { id_card: payload.user.image, step: 8 },
-  //           {
-  //             where: {
-  //               user_id: id,
-  //             },
-  //           }
-  //         );
-
-  //         response = otherResponse.picture;
-  //       } else if (payload.type === "image" && stage?.step === 8) {
-  //         await update(
-  //           { picture: payload.user.image, step: 9 },
-  //           {
-  //             where: {
-  //               user_id: id,
-  //             },
-  //           }
-  //         );
-
-  //         // await createArtisan(toSave);
-  //         // let acct_no = await Math.floor(Math.random() * 899999 + 1000000000);
-  //         const { data } = await AccountDetail(stage.full_name, payload.user.id);
-  //         // console.log(stage.full_name, id);
-  //         // const us = await currentStage(id);
-  //         await update(
-  //           { local_government: data },
-  //           {
-  //             where: {
-  //               user_id: payload.user.id,
-  //             },
-  //           }
-  //         );
-  //         const summary =
-  //           stage.menu === "Render Service (Artisan)" &&
-  //           `\n *Name* ${stage.full_name} *Service* ${stage.service} *State* ${
-  //             stage.state
-  //           } *Lga* ${stage.lga} *Address&* ${
-  //             stage.address
-  //           }. \n \n To complete your registration, kindly make a payment of ${account.formatMoney(
-  //             Number(data.data.amount),
-  //             "₦"
-  //           )} into * ${
-  //             data.data.account_number + " " + data.data.bank_name
-  //           }  *.  \n After payment, click the button below to confirm your payment`;
-  //         const header = "Here is the summary of your registration";
-  //         const button = [
-  //           {
-  //             type: "reply",
-  //             reply: { id: `${1}`, title: "Confirm payment" },
-  //           },
-  //         ];
-  //         response = productsButtons({ header, summary }, button);
-  //         // await destroy({ where: { user_id: id } });
-  //       } else if (
-  //         payload.text.toString() === "1" &&
-  //         payload.type === "text" &&
-  //         stage?.step === 9
-  //       ) {
-  //         const newAn = await currentStage(id);
-  //         const payment = await confirmPayment(
-  //           newAn.local_government.data.flw_ref
-  //         );
-  //         if (payment.status) {
-  //           const newArtisan = await currentStage(id);
-  //           const {
-  //             user_id,
-  //             menu,
-  //             full_name,
-  //             service,
-  //             state,
-  //             lga,
-  //             address,
-  //             id_card,
-  //             picture,
-  //           } = newArtisan;
-  //           const toSave = {
-  //             user_id,
-  //             menu,
-  //             full_name,
-  //             service,
-  //             state,
-  //             lga,
-  //             address,
-  //             id_card,
-  //             picture,
-  //           };
-
-  //           await createArtisan(toSave);
-  //           response = "Congratulation, your registration has been completed";
-  //         } else {
-  //           const {
-  //             local_government: { data: amount, account_number, bank_name },
-  //           } = await currentStage(id);
-  //           const summary =
-  //             stage.menu === "Render Service (Artisan)" &&
-  //             `\n *Name* ${stage.full_name} *Service* ${stage.service} *State* ${
-  //               stage.state
-  //             } *Lga* ${stage.lga} *Address&* ${
-  //               stage.address
-  //             }. \n \n To complete your registration, kindly make a payment of ${account.formatMoney(
-  //               Number(amount),
-  //               "₦"
-  //             )} into * ${
-  //               account_number + " " + bank_name
-  //             }  *.  \n After payment, click the button below to confirm your payment`;
-  //           const header = "Here is the summary of your registration";
-  //           const button = [
-  //             {
-  //               type: "reply",
-  //               reply: { id: `${1}`, title: "Confirm payment" },
-  //             },
-  //           ];
-  //           response = productsButtons({ header, summary }, button);
-  //         }
-  //       }
-  //     } else if (stage.menu === "Request Service Provider(Customer)") {
-  //       if (typeof text === "string" && stage?.step === 2 && type === "text") {
-  //         await update(
-  //           { full_name: text, step: 4 },
-  //           {
-  //             where: {
-  //               user_id: id,
-  //             },
-  //           }
-  //         );
-  //         response = await serviceResponse();
-  //       }
-
-  //       if (
-  //         type === "text" &&
-  //         stage?.menu === "Request Service Provider(Customer)" &&
-  //         stage?.step === 4
-  //       ) {
-  //         await update(
-  //           { step: 5 },
-  //           {
-  //             where: {
-  //               user_id: id,
-  //             },
-  //           }
-  //         );
-  //         response = otherResponse.address;
-  //       } else if (
-  //         type === "text" &&
-  //         stage?.menu === "Request Service Provider(Customer)" &&
-  //         stage?.step === 5
-  //       ) {
-  //         await update(
-  //           { address: text, step: 6 },
-  //           {
-  //             where: {
-  //               user_id: id,
-  //             },
-  //           }
-  //         );
-  //         response = otherResponse.location;
-  //       } else if (
-  //         type === "location" &&
-  //         stage?.menu === "Request Service Provider(Customer)" &&
-  //         stage?.step === 6
-  //       ) {
-  //         await update(
-  //           { location: image, step: 7 },
-  //           {
-  //             where: {
-  //               user_id: id,
-  //             },
-  //           }
-  //         );
-  //         response = otherResponse.task_description;
-  //       } else if (
-  //         type === "text" &&
-  //         stage?.menu === "Request Service Provider(Customer)" &&
-  //         stage?.step === 7
-  //       ) {
-  //         await update(
-  //           { task_description: text, step: 8 },
-  //           {
-  //             where: {
-  //               user_id: id,
-  //             },
-  //           }
-  //         );
-  //         let { artisans } = await getListOfArtisan();
-  //         await update(
-  //           { local_government: artisans },
-  //           {
-  //             where: {
-  //               user_id: id,
-  //             },
-  //           }
-  //         );
-
-  //         response = await artisanResponse();
-  //       } else if (
-  //         type === "text" &&
-  //         stage?.menu === "Request Service Provider(Customer)" &&
-  //         stage?.step === 8
-  //       ) {
-  //         const newCusto = await currentStage(id);
-  //         await update(
-  //           {
-  //             artisan: newCusto.local_government.includes(
-  //               newCusto.local_government[Number(text) - 1]
-  //             ).name,
-  //             step: 9,
-  //           },
-  //           {
-  //             where: {
-  //               user_id: id,
-  //             },
-  //           }
-  //         );
-  //         const newCustomerRequest = await currentStage(id);
-  //         const {
-  //           user_id,
-  //           menu,
-  //           full_name,
-  //           service,
-  //           address,
-  //           location,
-  //           task_description,
-  //           artisan,
-  //         } = newCustomerRequest;
-  //         const requestToSave = {
-  //           user_id,
-  //           menu,
-  //           full_name,
-  //           service,
-  //           address,
-  //           location,
-  //           task_description,
-  //           artisan,
-  //         };
-  //         await saveCustomerRequest(requestToSave);
-  //         await destroy({ where: { user_id: id } });
-  //         response = "Your request has been received successfully";
-  //       }
-  //     } else {
-  //       console.log(type, text);
-  //       response =
-  //         "Invalid input,please check or enter *[restart]* to start all over";
-  //     }
-  //     return res.status(200).json(response);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
 };
