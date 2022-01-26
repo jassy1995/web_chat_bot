6237c1b994c63386c61f04b076a1bf32d62963a5
jassy1995 <babatundejoseph85@gmail.com>
2022-01-26 13:02:49 +0100

    i added payment status column

--- models/artisan_complete.model.js
+++ models/artisan_complete.model.js
@@ -1,48 +1,52 @@
 const ArtisanComplete = (sequelize, DataTypes, Sequelize) =>
-  sequelize.define("artisan_complete", {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: Sequelize.UUIDV4,
-      primaryKey: true,
-      allowNull: false,
+  sequelize.define(
+    "artisan_complete",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      user_id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      full_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      service: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      state: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lga: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      address: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      id_card: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      picture: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      payment_status: {
+        type: DataTypes.STRING,
+        defaultValue: "pending",
+      },
     },
-    user_id: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    full_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    service: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    state: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    lga: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    address: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    id_card: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    picture: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    payment_status: {
-      type: DataTypes.STRING,
-      defaultValue: "pending",
-    },
-  });
+    { timestamps: false }
+  );
 
 module.exports = ArtisanComplete;
