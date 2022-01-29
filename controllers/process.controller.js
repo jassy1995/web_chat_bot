afe4dde229722172bd4e1926a1de0facdb04baef
jassy1995 <babatundejoseph85@gmail.com>
2022-01-29 21:40:52 +0100

    edited

--- controllers/process.controller.js
+++ controllers/process.controller.js
@@ -55,6 +55,7 @@ exports.RegistrationProcess = async (req, res) => {
     const artisanOne = await getArtisanOne(payload.user.id);
     const acct_value = await AccountDetail(stage?.full_name, payload.user.id);
     const nextV = await AccountDetail(artisanOne?.full_name, payload.user.id);
+    const checkExistCustomer = await getExistCustomer(payload.user.id);
 
     if (payload.type === "text" && payload?.text?.toLowerCase() == "hi") {
       if (
@@ -389,23 +390,23 @@ exports.RegistrationProcess = async (req, res) => {
           }
         );
 
-        const summary = `Name: ${stage.full_name}, Service: ${
-          stage.service
-        }, State: ${stage.state}, LGA: ${stage.lga}, Address: ${
-          stage.address
-        } .Congrats!, your detail has been submitted,to complete your registration, kindly make a payment of *${account.formatMoney(
-          Number(acct_value.data?.amount),
-          "₦"
-        )}* into  *${acct_value.data?.account_number}* *${
-          acct_value.data?.bank_name
-        }*. After payment, click the button below to confirm your payment`;
-        const header = "Here is the summary of your registration";
-        const button = [
-          {
-            type: "reply",
-            reply: { id: `${1}`, title: "Confirm payment" },
-          },
-        ];
+        // const summary = `Name: ${stage.full_name}, Service: ${
+        //   stage.service
+        // }, State: ${stage.state}, LGA: ${stage.lga}, Address: ${
+        //   stage.address
+        // } .Congrats!, your detail has been submitted,to complete your registration, kindly make a payment of *${account.formatMoney(
+        //   Number(acct_value.data?.amount),
+        //   "₦"
+        // )}* into  *${acct_value.data?.account_number}* *${
+        //   acct_value.data?.bank_name
+        // }*. After payment, click the button below to confirm your payment`;
+        // const header = "Here is the summary of your registration";
+        // const button = [
+        //   {
+        //     type: "reply",
+        //     reply: { id: `${1}`, title: "Confirm payment" },
+        //   },
+        // ];
         const toSave = {
           user_id: stage.user_id,
           full_name: stage?.full_name,
@@ -419,56 +420,73 @@ exports.RegistrationProcess = async (req, res) => {
         };
 
         await createArtisan(toSave);
-        let re = productsButtons({ header, summary }, button);
-        response = await sendResponse(re, payload.user.id);
-      } else if (
-        payload.text.toString() === "1" &&
-        payload.type === "text" &&
-        stage?.step === 10
-      ) {
-        const payment = await confirmPayment(
-          JSON.parse(stage.local_government)?.flw_ref
-        );
-        if (payment?.data?.status) {
-          await updateArtisan(
-            {
-              payment_status: "paid",
-            },
-            {
-              where: {
-                user_id: payload.user.id,
-              },
-            }
-          );
-
-          let resp = "Congratulation, your payment  has been received";
-          response = await sendResponse(resp, payload.user.id);
-        } else {
-          const newData = await currentStage(payload.user.id);
-          const summary2 = `kindly make a payment of *${account.formatMoney(
-            Number(JSON.parse(newData?.local_government)?.amount),
-            "₦"
-          )}* into *${
-            JSON.parse(newData?.local_government)?.account_number
-          }* *${
-            JSON.parse(newData?.local_government)?.bank_name
-          }* .After payment, click the button below to confirm your payment`;
-          const header = "Hay,your payment has not been received.";
-          const button2 = [
-            {
-              type: "reply",
-              reply: { id: `${1}`, title: "Confirm payment" },
-            },
-          ];
-          let rr = productsButtons({ header, summary: summary2 }, button2);
-          response = await sendResponse(rr, payload.user.id);
-        }
-      } else {
+        // let re = productsButtons({ header, summary }, button);
+        // response = await sendResponse(re, payload.user.id);
+        let hhs = `Congratulation!, your registration has been completed,\n here is the summary of your information\n Name: *${stage?.full_name} \n Service: ${stage?.service} \n State: ${stage?.state} \n  local_government: ${stage?.lga} \n Address: ${stage?.address}`;
+        response = await sendResponse(hhs, payload.user.id);
+      }
+      // else if (
+      //   payload.text.toString() === "1" &&
+      //   payload.type === "text" &&
+      //   stage?.step === 10
+      // ) {
+      // const payment = await confirmPayment(
+      //   JSON.parse(stage.local_government)?.flw_ref
+      // );
+      // if (payment?.data?.status) {
+      //   await updateArtisan(
+      //     {
+      //       payment_status: "paid",
+      //     },
+      //     {
+      //       where: {
+      //         user_id: payload.user.id,
+      //       },
+      //     }
+      //   );
+      //   let resp = "Congratulation, your payment  has been received";
+      //   response = await sendResponse(resp, payload.user.id);
+      // }
+      // else {
+      //   const newData = await currentStage(payload.user.id);
+      //   const summary2 = `kindly make a payment of *${account.formatMoney(
+      //     Number(JSON.parse(newData?.local_government)?.amount),
+      //     "₦"
+      //   )}* into *${
+      //     JSON.parse(newData?.local_government)?.account_number
+      //   }* *${
+      //     JSON.parse(newData?.local_government)?.bank_name
+      //   }* .After payment, click the button below to confirm your payment`;
+      //   const header = "Hay,your payment has not been received.";
+      //   const button2 = [
+      //     {
+      //       type: "reply",
+      //       reply: { id: `${1}`, title: "Confirm payment" },
+      //     },
+      //   ];
+      //   let rr = productsButtons({ header, summary: summary2 }, button2);
+      //   response = await sendResponse(rr, payload.user.id);
+      // }
+      // }
+      else {
         let rq =
           "Invalid input,please check and retry or enter *restart* to start all over";
         response = await sendResponse(rq, payload.user.id);
       }
     } else if (stage?.menu === "Request Service Provider(Customer)") {
+      // checkExistCustomer;
+      // if (payload?.text?.toString() === "1" && stage?.step === 3 && checkExistCustomer.user_id===payload.user.id) {
+      //    await update(
+      //      { full_name: payload.user.name, step: 4 },
+      //      {
+      //        where: {
+      //          user_id: payload.user.id,
+      //        },
+      //      }
+      //    );
+      //    let ttt = await serviceResponse();
+      //    response = await sendResponse(ttt, payload.user.id);
+      // }
       if (payload?.text?.toString() === "1" && stage?.step === 3) {
         await update(
           { full_name: payload.user.name, step: 4 },
