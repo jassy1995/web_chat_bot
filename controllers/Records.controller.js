ba9d4fa051ca6fbcb1dc95b53f59b5816c506b87
jassy1995 <babatundejoseph85@gmail.com>
2022-02-13 09:04:54 +0100

    added service endpoint

--- controllers/Records.controller.js
+++ controllers/Records.controller.js
@@ -1,4 +1,4 @@
-const { ArtisanComplete, CustomerComplete, Service } = require("../models");
+const { ArtisanComplete, CustomerComplete } = require("../models");
 
 exports.getRecords = async (req, res) => {
   try {
@@ -11,44 +11,44 @@ exports.getRecords = async (req, res) => {
 };
 
 //store service data into database
-exports.savedServiceRecords = async (req, res) => {
-  let services = [
-    { service_name: "Carpenter" },
-    { service_name: "Plumber" },
-    { service_name: "AC Repairs" },
-    { service_name: "Furniture" },
-    { service_name: "washing machine repair" },
-    { service_name: "Generator service and repair" },
-    { service_name: "Electrical Service" },
-    { service_name: "Satellite/dstv service" },
-    { service_name: "Refrigerator repair" },
-    { service_name: "Aluminium fabrication" },
-    { service_name: "Metal fabrication(welding)" },
-    { service_name: "Masonyry" },
-    { service_name: "Gardening" },
-    { service_name: "Cleaning" },
-    { service_name: "Laundary services" },
-    { service_name: "Fumigation" },
-    { service_name: "Painting" },
-    { service_name: "Tiling" },
-    { service_name: "Waiter" },
-    { service_name: "Driver" },
-    { service_name: "Cook" },
-    { service_name: "Security" },
-    { service_name: "Administrative officer" },
-    { service_name: "Barber" },
-    { service_name: "Dispatch rider" },
-  ];
-  try {
-    let response;
-    services.forEach(async (service, index) => {
-      const { service_name } = service;
-      const dataTOSave = { service_name };
-      response = await Service.create(dataTOSave);
-    });
+// exports.savedServiceRecords = async (req, res) => {
+//   let services = [
+//     { service_name: "Carpenter" },
+//     { service_name: "Plumber" },
+//     { service_name: "AC Repairs" },
+//     { service_name: "Furniture" },
+//     { service_name: "washing machine repair" },
+//     { service_name: "Generator service and repair" },
+//     { service_name: "Electrical Service" },
+//     { service_name: "Satellite/dstv service" },
+//     { service_name: "Refrigerator repair" },
+//     { service_name: "Aluminium fabrication" },
+//     { service_name: "Metal fabrication(welding)" },
+//     { service_name: "Masonyry" },
+//     { service_name: "Gardening" },
+//     { service_name: "Cleaning" },
+//     { service_name: "Laundary services" },
+//     { service_name: "Fumigation" },
+//     { service_name: "Painting" },
+//     { service_name: "Tiling" },
+//     { service_name: "Waiter" },
+//     { service_name: "Driver" },
+//     { service_name: "Cook" },
+//     { service_name: "Security" },
+//     { service_name: "Administrative officer" },
+//     { service_name: "Barber" },
+//     { service_name: "Dispatch rider" },
+//   ];
+//   try {
+//     let response;
+//     services.forEach(async (service, index) => {
+//       const { service_name } = service;
+//       const dataTOSave = { service_name };
+//       response = await Service.create(dataTOSave);
+//     });
 
-    return res.json(response);
-  } catch (error) {
-    return res.status(500).json({ message: "error occur", error });
-  }
-};
+//     return res.json(response);
+//   } catch (error) {
+//     return res.status(500).json({ message: "error occur", error });
+//   }
+// };
--- routes/process.route.js
+++ routes/process.route.js
@@ -2,13 +2,13 @@ const express = require("express");
 const { RegistrationProcess } = require("../controllers/process.controller");
 const {
   getRecords,
-  savedServiceRecords,
+  // savedServiceRecords,
 } = require("../controllers/Records.controller");
 
 const router = express.Router();
 
 router.post("/api/artisan/registration/process", RegistrationProcess);
 router.get("/api/all_records", getRecords);
-router.post("/api/save-service", savedServiceRecords);
+// router.post("/api/save-service", savedServiceRecords);
 
 module.exports = router;
